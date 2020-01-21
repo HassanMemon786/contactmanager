e9c266a8b3cff64b9f4e7a3dc3526cc411147cf7
HassanMemon786 <hassanlakhani3@gmail.com>
2020-01-21 14:59:51 -0800

    Updates

---
+++ precache-manifest.1845bc17955797a10e6fe2235827e4c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64848b59d601c8d6294c60507a866260",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "51e845b082bc1b499de4",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6e32e6f197af2bd8ff66",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "51e845b082bc1b499de4",
+    "url": "/contactmanager/static/js/2.bcf66a0b.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.bcf66a0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e32e6f197af2bd8ff66",
+    "url": "/contactmanager/static/js/main.06d52753.chunk.js"
+  },
+  {
+    "revision": "c9b248b7ef52cebe72fa",
+    "url": "/contactmanager/static/js/runtime-main.bc9037e5.js"
+  }
+]);
